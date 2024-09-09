145bb39aedadf747bff15a991c6e27d2de7b8adc
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-09-09 12:21:42 -0300

    implementacao do flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.flywaydb:flyway-mysql")
+//	implementation("javax.xml.bind:jaxb-api:2.3.0")
 }
 
 kotlin {
